9e38faeb8f31bb2ec2a75df2049d04e1cd36b9ae
aditya-raulji <-email@example.com>
2024-12-13 13:09:53 +0530

    ehgs

--- src/loader/Loading.jsx
+++ src/loader/Loading.jsx
@@ -4,7 +4,24 @@ function Loading() {
   return (
     <div>
       <span className="flex items-center justify-center h-screen text-3xl">
-        ...
+      <div className=" flex items-end justify-end h-screen "><div class="flex flex-col items-center space-y-4">
+      <div class="relative flex items-center justify-center w-16 h-16">
+      <div class="absolute w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+      <p class="text-lg font-semibold text-gray-300 animate-pulse">Loading...</p>
+      </div></div>
+     <img src="https://cdn.dribbble.com/users/2077073/screenshots/8069486/media/784c639cb9cc22970a3c180cad8d15a4.gif"  alt="" />
+        
+        <div className=" flex items-end justify-end h-screen"><div class="flex flex-col items-center space-y-4">
+      <div class="relative flex items-center justify-center w-16 h-16">
+      <div class="absolute w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+      <p class="text-lg font-semibold text-gray-300 animate-pulse">Loading...</p>
+      </div></div>
+
+
+  
+
       </span>
     </div>
   );
